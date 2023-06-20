595364f40fa5ee259d717ce5637173beb90f879d
kevintao2011 <36256475+kevintao2011@users.noreply.github.com>
2023-06-20 22:43:54 +0800

    doing create activity

--- src/utils/getproduct.js
+++ src/utils/getproduct.js
@@ -5,6 +5,7 @@ import user from "../models/user.js";
 import activity from "../models/activity.js";
 import { DBRef, ObjectId } from "mongodb";
 
+// for show product in carousell 
 const getProduct = async (req)=>{
     var connect;
     var jwt;
@@ -13,23 +14,27 @@ const getProduct = async (req)=>{
         connect = await mongoose.connect(String(process.env.CONNECTION_STRING));
                 
         
-        const productList = await product.find({})
+        const productList = await product.find({},{"_id":0 , "activity_id":0})
+        
+        
 
 
-        async function update (connect,productList) {
-            var newproductList = []
-            for (const doc of productList) {
-                console.log("productList",doc)
-                const act =  await activity.findById(doc.get("activity").toString(),{"activity_name":1})
-                doc.set("activity", act.get("activity_name"))
-                console.log("act",doc)
-                newproductList.push(doc)
-            }
-            connect.disconnect()
-            return newproductList
-        }
+        // // when activity is objectID
+        // async function update (connect,productList) {
+        //     var newproductList = []
+        //     for (const doc of productList) {
+        //         console.log("productList",doc)
+        //         const act =  await activity.findById(doc.get("activity").toString(),{"activity_name":1})
+        //         doc.set("activity", act.get("activity_name"))
+        //         console.log("act",doc)
+        //         newproductList.push(doc)
+        //     }
+        //     connect.disconnect()
+        //     return newproductList
+        // }
 
-        return await update(connect,productList)
+        // return await update(connect,productList)
+        
         return productList
     } catch (err) {
         console.log("error",err);
