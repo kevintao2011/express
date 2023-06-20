cf70e464887da4f6c1a1f6b48c0bdca7407623d0
kevintao2011 <36256475+kevintao2011@users.noreply.github.com>
2023-06-20 16:56:11 +0800

    update

---
+++ src/utils/addProduct.js
@@ -0,0 +1,39 @@
+
+import mongoose, { connect } from "mongoose";
+import connectDB from "./connectDB.js";
+import user from "../models/user.js";
+
+const findUser = async (req)=>{
+    var connect;
+    var data;
+    try {
+        connect = await mongoose.connect(String(process.env.CONNECTION_STRING));
+        data = req.body;
+        console.log(data);
+
+        const newUser = new user({
+            email:data.email,
+            cohort:data.cohort,
+            major:data.major,
+            gender:data.gender,
+            contact:data.contact,
+            username:data.username,
+        })
+        await user.create(newUser);
+        
+        return 
+
+    } catch (err) {
+        console.log("error",err);
+        console.log("failed");
+        // connect.disconnect();
+
+    }
+    try{
+
+    }catch(err){
+        console.log("error",err);
+    }
+}
+
+export default findUser;
