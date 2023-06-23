03c3c7f511f44fbfee7f80a775c2d0c32dd41760
kevintao2011 <36256475+kevintao2011@users.noreply.github.com>
2023-06-23 18:44:36 +0800

    cors issue

--- src/app.js
+++ src/app.js
@@ -8,12 +8,25 @@ import tryConnectDB from "./utils/tryConnectDB.js";
 import getinfo from "./utils/info/major.js";
 import getProduct from "./utils/getproduct.js";
 import createProduct from "./utils/createproduct.js";
+import { loginfirebase } from "./utils/firebasefunction.js";
+import cors from "cors";
+import https from "https"
+import http from "http"
+import fs from "fs";
 
 const app = express()
-const port = 3000
+const port = 3001
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+var corsOptions = {
+  origin: "localhost:3000",
+  optionsSuccessStatus: 200 
+}
+
+app.use(cors(corsOptions))
+app.use(()=>{"get req"})
 
 app.get('/', async (req, res) => {
     await tryConnectDB()
@@ -60,7 +73,35 @@ app.post('/createproduct',async (req, res) => {
   res.send(result)
 })
 
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.post('/signin',async (req, res) => {
+  const data = req.body
+  console.log("credentials",data)
+  const result = await loginfirebase(req.body.email,req.body.password);  
+  res.send(result)
 })
+
+
+
+// https
+//   .createServer(
+//     // Provide the private and public key to the server by reading each
+//     // file's content with the readFileSync() method.
+//     {
+//       key: fs.readFileSync("./key.pem"),
+//       cert: fs.readFileSync("./cert.pem"),
+//     },
+//     app
+//   )
+//   .listen(port, () => {
+//     console.log(`serever is runing at port '${port}'`);
+//   });
+
+  http
+  .createServer(function (request, response) {
+    // Set the response HTTP header with HTTP status and Content type
+    response.writeHead(200, { "Content-Type": "text/plain" });
+
+    // Send the response body "Hello World"
+    response.end("Hello World\n");
+  })
+  .listen(port);
