0256248b163dba7c317620d42d3d34260cd9ee4b
kevintao2011 <36256475+kevintao2011@users.noreply.github.com>
2023-08-23 21:50:23 +0800

    update

--- src/models/transaction.js
+++ src/models/transaction.js
@@ -1,13 +1,14 @@
 
 import { Schema, model} from 'mongoose';
-
+import mongoose from 'mongoose';
+import {product,vari} from './product.js';
 
 
 const TransactionSchema = new Schema({ //schema
    
     payment_proof:String,
     payment_method:String,
-    productIds:Array,
+    productIds:[],
     products:Array,
     sid:String,
     email:String,
@@ -17,7 +18,8 @@ const TransactionSchema = new Schema({ //schema
     name:String,
     conversation:Array,
     log:Array,
-    create_at:Date
+    create_at:Date,
+    createdBy:{type:mongoose.SchemaTypes.ObjectId,ref:'users',immutable:true}
 });
 // ProductSchema.path('activity').ref(activity)
 const transaction = model("transactions", TransactionSchema);  //Creating a model
