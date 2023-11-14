5752c8121d1e49b696d17d52587ef57b02e42815
kevintao2011 <36256475+kevintao2011@users.noreply.github.com>
2023-11-14 16:57:22 +0800

    test

--- src/utils/stock/findSocietyStock.js
+++ src/utils/stock/findSocietyStock.js
@@ -4,11 +4,20 @@ import { ObjectId } from 'mongodb';
 import { getoidandsessionbycode } from '../serverFunction/getoidbycode.js';
 
 export default async function findSocietyStock(req){
-    await getoidandsessionbycode(req.body.data.code).then(async result=>{
-        const [oid,cuurentSession]=result
-        await stock.find({ref_society:oid},{},{$group:["ref_society","ref_product"]}).then(docs=>{
-            console.log(docs)
-        })
-    })
-    return {success:true,data:"testing"}
+    try{
+        return await getoidandsessionbycode(req.body.data.code).then(async result=>{
+            const [oid,cuurentSession]=result
+            return await stock.find({ref_society:oid},{},{$group:["ref_society","ref_product"]}).then(docs=>{
+                console.log(docs)
+                return {success:true,data:docs}
+            })
+            
+        }) 
+    }catch{
+        return {success:false,data:"failed to get stocks"}
+    }
+    
+
+   
+   
 }
