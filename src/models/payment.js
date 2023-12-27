50b0ac84d1231186c135033f9a2ffe0a76f66916
kevintao2011 <36256475+kevintao2011@users.noreply.github.com>
2023-12-27 15:50:37 +0800

    update

--- src/models/payment.js
+++ src/models/payment.js
@@ -2,13 +2,25 @@ import { ObjectId } from 'mongodb';
 import { Schema, model} from 'mongoose';
 
 
+
+
+const FPSSchema = new Schema({
+    qr_code_url:String,
+    phone_number:Number,
+    bank_account:{
+        index:String,
+        number:Number,   
+    },
+    shown_name:String,
+    activated:{type:String,default:false}
+})
+
 const PaymentMethodSchema = new Schema({ //schema
-
-    payment_name:String,
-    online:Boolean,
-    link:String,
-
+    ref_society:String,
+    FPS:FPSSchema,
+    
 });
+
 const paymentMethod = model("payments", PaymentMethodSchema);  //Creating a model
 
 export {PaymentMethodSchema,paymentMethod};
